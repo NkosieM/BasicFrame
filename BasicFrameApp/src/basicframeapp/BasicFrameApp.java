5ace309a110b0466f298691c7aabddba7c1b3058
NkosieM <123198465+NkosieM@users.noreply.github.com>
2025-10-31 00:48:47 +0200

    Add files via upload

---
+++ BasicFrameApp/src/basicframeapp/BasicFrameApp.java
@@ -0,0 +1,14 @@
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+public class BasicFrameApp {
+    
+    public static void main(String[] args) {
+
+        MyFirstFrame frame;
+        
+        frame = new MyFirstFrame();
+    }
+    
+}
